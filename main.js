21849014ae39b7c85b0e82960f13fd6ff36d9950
Bastien Fontagne <fontagne.bastien1@gmail.com>
2023-11-15 11:51:35 +0100

    commit initial

---
+++ main.js
@@ -0,0 +1,8 @@
+var MavariableString = "toto";
+var MavariableNumerique = prompt("entrer un nombre",100);
+
+if (MavariableString == "toto" && MavariableNumerique !=200 || MavariableNumerique <=100){
+    alert("je suis le cas 1");
+}else{
+    alert("je suis le cas 2");
+}
