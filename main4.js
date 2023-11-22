cdf1436d11418321ce0dc0153eb8e26f2be3c08c
Bastien Fontagne <fontagne.bastien1@gmail.com>
2023-11-22 09:48:57 +0100

    exo4 fini

---
+++ main4.js
@@ -0,0 +1,8 @@
+var MavariableString = "toto";
+var MavariableNumerique = prompt("Entrer un nombre", 100);
+
+if (MavariableString == "toto" && MavariableNumerique != 200 ||MavariableNumerique <= 100){
+    alert("Je suis le cas 1");
+} else {
+    alert("Je suis le cas 2");
+}
