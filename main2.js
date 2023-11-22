c31251ccc92dac93dcd51e91ebcee752e3aa4b5a
Bastien Fontagne <fontagne.bastien1@gmail.com>
2023-11-22 09:41:45 +0100

    exo2 fini

---
+++ main2.js
@@ -0,0 +1,6 @@
+var MavariableString = "toto";
+var MavariableNumerique = 100;
+var Mavariabletableau = ['Apple', 'Banana'];
+var MavaribleObjet = window
+
+alert (MavariableString + MavariableNumerique + Mavariabletableau[0] + MavaribleObjet.location.pathname);
