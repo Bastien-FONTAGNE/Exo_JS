61efecde819db07d9b9b365674f8a20924405764
Bastien Fontagne <fontagne.bastien1@gmail.com>
2023-11-22 11:16:06 +0100

    exo6 fini

--- main6.js
+++ main6.js
@@ -1,5 +1,11 @@
 var MavariableNumérique = parseInt(prompt("écrire un nombre : "));
-var resultat = 1;
+
+if (MavariableNumérique < 0) {
+    alert("Le résultat est : 0");
+}
+
+let resultat = 1;
+
 
 for (var i = 1; i <= MavariableNumérique; i++)
 {
